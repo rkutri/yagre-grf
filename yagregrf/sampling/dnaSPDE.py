57fca32edbac7d9822c0b77e794279c5f2b8aebf
rKutri <r.kutri@gmail.com>
2025-04-09 17:12:24 +0200

    use direct solver as default

--- yagregrf/sampling/dnaSPDE.py
+++ yagregrf/sampling/dnaSPDE.py
@@ -6,19 +6,20 @@ from yagregrf.sampling.spde import SPDEEngine2d
 
 class DNASPDEEngine2d(SamplingEngine):
 
-    def __init__(self, corrLength, nu, nVertPerDim, alpha):
+    def __init__(self, variance, corrLength, nu, nVertPerDim, alpha):
 
         self._nGrid = nVertPerDim
         self._alpha = alpha
 
-        self._engines = [SPDEEngine2d(corrLength, nu, nVertPerDim, alpha, [False, False]),
-                         SPDEEngine2d(
-            corrLength, nu, nVertPerDim, alpha, [
-                False, True]),
-            SPDEEngine2d(
-            corrLength, nu, nVertPerDim, alpha, [
-                True, False]),
-            SPDEEngine2d(corrLength, nu, nVertPerDim, alpha, [True, True])]
+        self._engines = [
+            SPDEEngine2d(variance, corrLength, nu, nVertPerDim, alpha,
+                [False, False]),
+            SPDEEngine2d(variance, corrLength, nu, nVertPerDim, alpha,
+                [False, True]),
+            SPDEEngine2d(variance, corrLength, nu, nVertPerDim, alpha,
+                [True, False]),
+            SPDEEngine2d(variance, corrLength, nu, nVertPerDim, alpha,
+                [True, True])]
 
     @property
     def mesh(self):
--- yagregrf/sampling/spde.py
+++ yagregrf/sampling/spde.py
@@ -1,34 +1,24 @@
 import numpy as np
-import fenics as fncs
-
+import dolfin as df
 from scipy.special import gamma
 from numpy.random import standard_normal
-
 from yagregrf.sampling.interface import SamplingEngine
 
 
 def on_dirichlet_boundary_2d(x, dir0, dir1, delta, TOL=1e-8):
-
-    if (dir0):
-        if (x[0] + delta < TOL or x[0] > 1. + delta - TOL):
+    if dir0:
+        if x[0] + delta < TOL or x[0] > 1. + delta - TOL:
             return True
-
-    if (dir1):
-        if (x[1] + delta < TOL or x[1] > 1. + delta - TOL):
+    if dir1:
+        if x[1] + delta < TOL or x[1] > 1. + delta - TOL:
             return True
-
     return False
 
 
 def white_noise_factor(M, V):
-
     H = np.zeros(V.dim())
-    H[0] = np.sum(M.getrow(0)[1])
-    H[V.dim() - 1] = np.sum(M.getrow(V.dim() - 1)[1])
-
-    for i in range(1, V.dim() - 1):
+    for i in range(V.dim()):
         H[i] = np.sum(M.getrow(i)[1])
-
     return 1. / np.sqrt(H)
 
 
@@ -36,15 +26,16 @@ class SPDEEngine2d(SamplingEngine):
 
     DIM = 2
 
-    def __init__(self, corrLength, nu, nVertPerDim,
-                 alpha, useDirBC=[False, False]):
+    def __init__(self, variance, corrLength, nu, nVertPerDim, alpha,
+                 useDirBC=[False, False], useDirectSolver=True):
 
-        fncs.set_log_level(fncs.LogLevel.ERROR)
+        df.set_log_level(df.LogLevel.ERROR)
 
+        self._sd = np.sqrt(variance)
         self._nGrid = nVertPerDim
 
         beta = 0.5 * nu + 0.25 * SPDEEngine2d.DIM
-        self._betaInt = int(np.rint(0.5 * nu + 0.25 * SPDEEngine2d.DIM))
+        self._betaInt = int(np.rint(beta))
 
         if np.abs(beta - self._betaInt) > 1e-8:
             raise NotImplementedError(
@@ -53,49 +44,63 @@ class SPDEEngine2d(SamplingEngine):
             raise ValueError(
                 f"Received invalid correlation length: {corrLength}")
         if nu < 0.5:
-            raise ValueError(
-                f"Received invalid smoothness parameter: {nu}")
+            raise ValueError(f"Received invalid smoothness parameter: {nu}")
 
         kappa = np.sqrt(2. * nu) / corrLength
-
-        # oversamling width
         delta = 0.5 * (alpha - 1.)
 
-        lowerLeft = fncs.Point(-delta, -delta)
-        upperRight = fncs.Point(1. + delta, 1. + delta)
+        lowerLeft = df.Point(-delta, -delta)
+        upperRight = df.Point(1. + delta, 1. + delta)
 
-        self._mesh = fncs.RectangleMesh(
-            lowerLeft, upperRight, nVertPerDim - 1, nVertPerDim - 1)
+        self._mesh = df.RectangleMesh(
+            lowerLeft,
+            upperRight,
+            nVertPerDim - 1,
+            nVertPerDim - 1)
         self._varScaling = np.sqrt(
-            4. * np.pi * gamma(1. + nu) / gamma(nu) * np.power(kappa, 2. * nu))
+            4. * np.pi * gamma(1. + nu) / gamma(nu) * np.power(kappa, 2. * nu)
+        )
 
-        self._V = fncs.FunctionSpace(self._mesh, "Lagrange", 1)
+        self._V = df.FunctionSpace(self._mesh, "Lagrange", 1)
 
-        u = fncs.TrialFunction(self._V)
-        v = fncs.TestFunction(self._V)
-        R = fncs.Constant(kappa * kappa)
+        u = df.TrialFunction(self._V)
+        v = df.TestFunction(self._V)
+        R = df.Constant(kappa * kappa)
 
-        a = fncs.inner(fncs.grad(u), fncs.grad(v)) * \
-            fncs.dx + R * u * v * fncs.dx
+        a = df.inner(df.grad(u), df.grad(v)) * df.dx + R * u * v * df.dx
+        dirBCVal = df.Constant(0.)
 
-        dirBCVal = fncs.Constant(0.)
+        self._bc = df.DirichletBC(
+            self._V,
+            dirBCVal,
+            lambda x: on_dirichlet_boundary_2d(
+                x, useDirBC[0], useDirBC[1], delta)
+        )
 
-        self._bc = fncs.DirichletBC(
-            self._V, dirBCVal, lambda x: on_dirichlet_boundary_2d(
-                x, useDirBC[0], useDirBC[1], delta))
+        self._A = df.assemble(a)
+        self._A.ident_zeros()
+        self._bc.apply(self._A)
 
-        A = fncs.assemble(a)
-        self._bc.apply(A)
+        if useDirectSolver:
 
-        self._solver = fncs.LUSolver(A, 'umfpack')
-        self._solver.parameters['symmetric'] = True
+            self._solver = df.LUSolver("umfpack")
+            self._solver.parameters["reuse_factorization"] = True
+            self._solver.set_operator(self._A)
 
-        m = u * v * fncs.dx
+        else:
 
-        M = fncs.assemble(m)
-        self._bc.apply(M)
+            self._solver = df.KrylovSolver("cg", "hypre_amg")
+            self._solver.set_operator(self._A)
 
+        m = u * v * df.dx
+        M = df.assemble(m)
         self._H = white_noise_factor(M, self._V)
+
+        self._sol = df.Function(self._V)
+        self._f = df.Function(self._V)
+        self._l = self._f * v * df.dx
+        self._b = df.Vector()
+        df.assemble(self._l, tensor=self._b)
 
     @property
     def mesh(self):
@@ -103,35 +108,26 @@ class SPDEEngine2d(SamplingEngine):
 
     def generate_realisation(self):
 
-        fncs.set_log_level(fncs.LogLevel.ERROR)
+        fDofs = self._f.vector().get_local()
+        fDofs[:] = self._sd * self._varScaling * self._H * standard_normal(self._V.dim())
+        self._f.vector().set_local(fDofs)
+        self._f.vector().apply("insert")
 
-        v = fncs.TestFunction(self._V)
+        df.assemble(self._l, tensor=self._b)
+        self._bc.apply(self._b)
 
-        f = fncs.Function(self._V)
-        u = fncs.Function(self._V)
-
-        fDofs = self._varScaling * self._H * standard_normal(self._V.dim())
-        f.vector()[:] = fDofs
-
-        l = f * v * fncs.dx
-        b = fncs.assemble(l)
-
-        self._solver.solve(u.vector(), b)
+        self._solver.solve(self._sol.vector(), self._b)
 
         biCounter = self._betaInt
+        while biCounter > 1:
 
-        while (biCounter > 1):
+            self._f.assign(self._sol)
 
-            rhs = fncs.Function(self._V)
-            rhs.assign(u)
+            df.assemble(self._l, tensor=self._b)
+            self._bc.apply(self._b)
 
-            l = rhs * v * fncs.dx
-            b = fncs.assemble(l)
-            self._bc.apply(b)
-
-            self._solver.solve(u.vector(), b)
-
+            self._solver.solve(self._sol.vector(), self._b)
             biCounter -= 1
 
-        return u.compute_vertex_values(
+        return self._sol.compute_vertex_values(
             self._mesh).reshape(self._nGrid, self._nGrid)
