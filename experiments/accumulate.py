fix bug in marginal variance file generation

experiments/accumulate.py
@@ -1,51 +1,59 @@
 import os
 import csv
-
 import numpy as np
-
 from glob import glob
-
 from filename import create_data_string
-
 
 # Parameters
 DIM = 2
 var = 0.1
-ell = 0.2
-nu = 3.
-nSampBatch = int(5e1)
+ell = 0.05
+nu = 1.
+nSampBatch = int(1e1)
 
 errorTypes = ["maxError", "froError"]
 
-for error in errorTypes:
+baseDir = 'data'
+dataConfig = [("oversampling", "os"),
+              ("memory", "mem"),
+              ("cost", "cost"),
+              ("marginalVariance", "mv")]
 
-    baseDir = 'data'
-    dataConfig = [("oversampling", "os"),
-                  ("memory", "mem"),
-                  ("cost", "cost"),
-                  ("marginalVariance", "mv")]
+for subDir, prefix in dataConfig:
 
-    for subDir, prefix in dataConfig:
+    if prefix == "mv":
+        relevantErrors = [None]  # only process once
+    else:
+        relevantErrors = errorTypes
+
+    for error in relevantErrors:
 
         filenamePrefix = create_data_string(
             DIM, var, ell, nu, nSampBatch, prefix)
 
-        inDir = os.path.join(baseDir, subDir, error)
-        inDir = os.path.join(inDir, filenamePrefix)
+        if prefix == "mv":
+            inDir = os.path.join(baseDir, subDir, filenamePrefix)
+        else:
+            inDir = os.path.join(baseDir, subDir, error, filenamePrefix)
 
         filePattern = os.path.join(inDir, filenamePrefix + "_*.csv")
-
         csvFiles = glob(filePattern)
-
         nBatch = len(csvFiles)
 
-        outFilename = create_data_string(DIM, var, ell, nu, nSampBatch,
-                                         prefix + "_ACCUMULATED") + f"_{nBatch}batches_" + error + ".csv"
+        print(f"{subDir} has {nBatch} files")
 
         if not csvFiles:
             raise RuntimeError(
-                "No files found matching the pattern:\n" +
-                filePattern)
+                "No files found matching the pattern:\n" + filePattern)
+
+        if prefix == "mv":
+            outFilename = create_data_string(
+                DIM, var, ell, nu, nSampBatch,
+                prefix + "_ACCUMULATED") + f"_{nBatch}batches.csv"
+        else:
+            outFilename = create_data_string(
+                DIM, var, ell, nu, nSampBatch,
+                prefix + "_ACCUMULATED") + f"_{nBatch}batches_" + error + ".csv"
 
         methods = []
         variableData = {}
@@ -55,53 +63,39 @@ for error in errorTypes:
 
         for filename in csvFiles:
             with open(filename, 'r') as file:
-
                 reader = csv.reader(file)
                 rows = list(reader)
 
                 if prefix == "os":
-
-                    # first row contains the mesh widhts
+                    # first row contains mesh widths
                     meshWidths = [float(h) for h in rows[0][1:]]
                     variableData["meshWidths"] = meshWidths
 
                     for row in rows[1:]:
-
                         method = row[0]
-
                         if method not in methods:
                             methods.append(method)
-
                         if method not in errorSamples:
                             errorSamples[method] = []
-
                         errorSamples[method].append(
                             [float(x) for x in row[1:]])
 
                 elif prefix == "mv":
-
                     pos = [float(x) for x in rows[0][1:]]
                     variableData["position"] = pos
 
                     for row in rows[1:]:
-
                         method = row[0]
-
                         if method not in methods:
                             methods.append(method)
-
                         if method not in margVarSamples:
                             margVarSamples[method] = []
-
                         margVarSamples[method].append(
-                            [float(var) for var in row[1:]])
+                            [float(x) for x in row[1:]])
 
                 else:
-
                     for row in rows:
-
                         label = row[0].rsplit('_', 1)
-
                         method = label[0]
                         quantity = label[1]
 
@@ -109,62 +103,53 @@ for error in errorTypes:
                             methods.append(method)
 
                         if quantity == error:
-
                             if method not in errorSamples:
                                 errorSamples[method] = []
-
-                            errors = [float(value) for value in row[1:]]
-                            errorSamples[method].append(errors)
-
+                            errorSamples[method].append(
+                                [float(x) for x in row[1:]])
                         else:
                             variableData[method] = [float(x) for x in row[1:]]
 
         if prefix == "mv":
             averagedMargVar = {
-                method: np.mean(
-                    margVarSamples[method],
-                    axis=0).tolist() for method in methods}
+                method: np.mean(margVarSamples[method], axis=0).tolist()
+                for method in methods
+            }
         else:
-            averagedErrors = {method: np.mean(errorSamples[method], axis=0).tolist()
-                              for method in methods}
+            averagedErrors = {
+                method: np.mean(errorSamples[method], axis=0).tolist()
+                for method in methods
+            }
 
-            # confidence interval ~ 95%
-            ciFactor = 1.96
-            errorBars = {method: (ciFactor * np.std(errorSamples[method],
-                                                    axis=0)).tolist() for method in methods}
+            ciFactor = 1.96  # 95% confidence interval
+            errorBars = {
+                method: (
+                    ciFactor *
+                    np.std(
+                        errorSamples[method],
+                        axis=0)).tolist()
+                for method in methods
+            }
 
         # Write to output CSV
         with open(os.path.join(baseDir, outFilename), mode='w', newline='') as file:
-
             writer = csv.writer(file)
 
             if prefix == "os":
-
                 writer.writerow(["meshWidths"] + variableData["meshWidths"])
-
                 for method in methods:
-
                     writer.writerow([method] + averagedErrors[method])
-
-                    barRow = method + "_bars"
-                    writer.writerow([barRow] + errorBars[method])
+                    writer.writerow([method + "_bars"] + errorBars[method])
 
             elif prefix == "mv":
-
                 writer.writerow(["position"] + variableData["position"])
-
                 for method in methods:
                     writer.writerow([method] + averagedMargVar[method])
 
             else:
-
                 for method in methods:
-
-                    variableRow = method + "_" + subDir
-                    writer.writerow([variableRow] + variableData[method])
-
-                    errorRow = method + "_" + error
-                    writer.writerow([errorRow] + averagedErrors[method])
-
-                    barRow = errorRow.replace(error, "bars")
-                    writer.writerow([barRow] + errorBars[method])
+                    writer.writerow([method + "_" + subDir] +
+                                    variableData[method])
+                    writer.writerow([method + "_" + error] +
+                                    averagedErrors[method])
+                    writer.writerow([method + "_bars"] + errorBars[method])

experiments/spdeComparison.py
@@ -38,9 +38,9 @@ print(f"Filename ID set to: '{filenameID}'")
 # Parameters
 DIM = 2
 var = 0.1
-ell = 0.2
-nu = 3.
-nSamp = int(5e1)
+ell = 0.05
+nu = 1.
+nSamp = int(5e4)
 
 kappa = np.sqrt(2. * nu) / ell
 beta = 0.5 * (1. + nu)
@@ -346,18 +346,21 @@ experimentConfig = [
 
 errorTypes = ["maxError", "froError"]
 
+
 for dataVariable, prefix in experimentConfig:
 
-    dataString = create_data_string(DIM, var, ell, nu, nSamp, prefix)
+    for i, error in enumerate(errorTypes):
 
-    for error in errorTypes:
+        # Skip mv for all but the first error type
+        if prefix == "mv" and i > 0:
+            continue
 
-        paramDir = dataString
+        dataString = create_data_string(DIM, var, ell, nu, nSamp, prefix)
 
         if prefix == "mv":
-            outDir = os.path.join("data", dataVariable, paramDir)
+            outDir = os.path.join("data", dataVariable, dataString)
         else:
-            outDir = os.path.join("data", dataVariable, error, paramDir)
+            outDir = os.path.join("data", dataVariable, error, dataString)
 
         os.makedirs(outDir, exist_ok=True)
 
@@ -370,62 +373,52 @@ for dataVariable, prefix in experimentConfig:
             if prefix == "os":
 
                 writer.writerow(["mesh_widths"] + [1. / n for n in dofPerDim])
-                writer.writerow(
-                    ["DNA_fourier"] +
-                    osData[error]["DNA_fourier"])
+                writer.writerow(["DNA_fourier"] + osData[error]["DNA_fourier"])
                 writer.writerow(["DNA_spde"] + osData[error]["DNA_spde"])
 
                 for i, alpha in enumerate(oversampling):
                     writer.writerow(
                         [f"SPDE_alpha{int(100*alpha)}"] +
-                        osData[error]["SPDE"][i])
+                        osData[error]["SPDE"][i]
+                    )
 
-            if prefix == "mem":
+            elif prefix == "mem":
 
                 writer.writerow(
                     ["DNA_fourier_memory"] +
                     memData["DNA_fourier"]["memory"])
-                writer.writerow(
-                    ["DNA_fourier_" + error] +
-                    memData["DNA_fourier"][error])
-
+                writer.writerow(["DNA_fourier_" + error] +
+                                memData["DNA_fourier"][error])
                 writer.writerow(
                     ["DNA_spde_memory"] +
                     memData["DNA_spde"]["memory"])
                 writer.writerow(["DNA_spde_" + error] +
                                 memData["DNA_spde"][error])
-
                 writer.writerow(
                     ["SPDE_osFix_memory"] +
                     memData["SPDE_osFix"]["memory"])
-                writer.writerow(
-                    ["SPDE_osFix_" + error] +
-                    memData["SPDE_osFix"][error])
+                writer.writerow(["SPDE_osFix_" + error] +
+                                memData["SPDE_osFix"][error])
 
-            if prefix == "cost":
+            elif prefix == "cost":
 
                 writer.writerow(
                     ["DNA_fourier_cost"] +
                     cData["DNA_fourier"]["cost"])
-                writer.writerow(
-                    ["DNA_fourier_" + error] +
-                    cData["DNA_fourier"][error])
-
+                writer.writerow(["DNA_fourier_" + error] +
+                                cData["DNA_fourier"][error])
                 writer.writerow(["DNA_spde_cost"] + cData["DNA_spde"]["cost"])
                 writer.writerow(["DNA_spde_" + error] +
                                 cData["DNA_spde"][error])
-
                 writer.writerow(
                     ["SPDE_osFix_cost"] +
                     cData["SPDE_osFix"]["cost"])
-                writer.writerow(
-                    ["SPDE_osFix_" + error] +
-                    cData["SPDE_osFix"][error])
+                writer.writerow(["SPDE_osFix_" + error] +
+                                cData["SPDE_osFix"][error])
 
-            if prefix == "mv" and error == errorTypes[0]:
+            elif prefix == "mv":
 
                 writer.writerow(["position"] + mvData["pos"].tolist())
-
                 writer.writerow(
                     ["DNA_fourier"] +
                     mvData["DNA_fourier"].tolist())